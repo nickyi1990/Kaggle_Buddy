bde5a533f265d2596acf8c1237b2ee602f69c5d0
NickyYi <972775099@qq.com>
2017-10-31 13:47:47 +0800

    update

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup
 setup(
     name='kaggle-buddy',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['kaggle-buddy'],                  # The name of your scipt, and also the command you'll be using for calling it
     description='Many machine learning tools to boost your model, including stackers, encoders, and other feature engineering methods',
     url='https://github.com/NickYi1990/Kaggle_Buddy',
     author='鲲(China)',
     zip_safe=False,
     license='MIT',
-    packages=['kaggle_buddy']
+    packages=['kaggle_buddy'],
     author_email='972775099@qq.com'
 )
