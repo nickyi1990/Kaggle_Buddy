c07de820fdc4a5afbb30e70be1ecbad1ec57de41
NickyYi <972775099@qq.com>
2017-10-31 13:45:41 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.1',                          # Update the version number for new releases
     scripts=['kaggle-buddy'],                  # The name of your scipt, and also the command you'll be using for calling it
     description='Many machine learning tools to boost your model, including stackers, encoders, and other feature engineering methods',
-    url='https://github.com/NickYi1990/Kaggle_Buddy.git',
+    url='https://github.com/NickYi1990/Kaggle_Buddy',
     author='鲲(China)',
+    zip_safe=False,
+    license='MIT',
+    packages=['kaggle_buddy']
     author_email='972775099@qq.com'
 )
