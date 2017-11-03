d868603ea10a4e8b0014a98320686eac8d6fff49
NickyYi <972775099@qq.com>
2017-11-03 18:59:46 +0800

    update stacking class

--- Ensemble/KA_ensemble.py
+++ Ensemble/KA_ensemble.py
@@ -36,13 +36,18 @@ class ka_stacking_generalization(object):
 
            kf_5 = KFold(data_train.shape[0], n_folds=5, shuffle=True, random_state=1024)
         '''
-        self.X_train = X_train.values
-        self.X_test = X_test.values
-        self.y = y.values
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y = y
         self.kf_n = kf_n
         self.verbose = verbose
 
-    def run_lgbm_stacker(self, lgbm_params, num_boost_round):
+    def run_lgbm_stacker(self
+                         , lgbm_params
+                         , num_boost_round
+                         , early_stopping_rounds
+                         , lightgbm_verbose_eval
+                         , verbose=1):
         '''Stacking for xgboost
 
            Parameters
@@ -51,6 +56,10 @@ class ka_stacking_generalization(object):
                 lightgbm parameters
            num_boost_round: int
                 number of boosting rounds
+           early_stopping_rounds: int
+                number of early stopping round, if we do not want to use earlying stopping,
+                set ---> early_stopping_rounds > num_boost_round
+
 
            Return
            ------
@@ -58,7 +67,6 @@ class ka_stacking_generalization(object):
                             stacked training data
            S_test: numpy array
                             stacked testing data
-
            Example
            -------
             params = {"learning_rate": 0.1
@@ -76,28 +84,39 @@ class ka_stacking_generalization(object):
 
             S_train, S_test = run_lgbm_stacker(xgb_params, 741)
         '''
-        with tick_tock("add stats features", self.verbose):
-            d_test = lightgbm.Dataset(self.X_test)
+        with tick_tock("stacking", self.verbose):
             n_folds = self.kf_n.n_folds
             S_train = np.zeros_like(self.y)
-            S_test_i = np.zeros((self.y.shape[0], n_folds))
+            S_test_i = np.zeros((self.X_test.shape[0], n_folds))
 
             for i, (train_index, val_index) in enumerate(self.kf_n):
+                if verbose > 0:
+                    print('lightgbm is stacking fold {0} ....'.format(i+1))
                 X_train_cv, X_val_cv = self.X_train[train_index], self.X_train[val_index]
-                y_train_cv = self.y[train_index]
+                y_train_cv, y_valid_cv = self.y[train_index], self.y[val_index]
 
                 d_train_fold = lightgbm.Dataset(X_train_cv, y_train_cv)
-                d_val_fold = lightgbm.Dataset(X_val_cv)
+                d_val_fold = lightgbm.Dataset(X_val_cv, y_valid_cv)
 
-                model_fold = lightgbm.train(lgbm_params, dtrain=d_train_fold, num_boost_round=num_boost_round)
-                S_train[val_index] = model_fold.predict(d_val_fold)
-                S_test_i[:, i] = model_fold.predict(d_test)
+                model_fold = lightgbm.train(lgbm_params
+                                            , train_set=d_train_fold
+                                            , valid_sets=d_val_fold
+                                            , early_stopping_rounds=early_stopping_rounds
+                                            , verbose_eval=lightgbm_verbose_eval
+                                            , num_boost_round=num_boost_round)
+                S_train[val_index] = model_fold.predict(X_val_cv)
+                S_test_i[:, i] = model_fold.predict(self.X_test)
 
             S_test = S_test_i.sum(axis=1) / n_folds
 
             return S_train, S_test
 
-    def run_xgboost_stacker(self, xgb_params, num_boost_round):
+    def run_xgboost_stacker(self
+                            , xgb_params
+                            , num_boost_round
+                            , early_stopping_rounds
+                            , xgboost_verbose_eval
+                            , verbose=1):
         '''Stacking for xgboost
 
            Parameters
@@ -129,26 +148,84 @@ class ka_stacking_generalization(object):
 
             S_train, S_test = run_xgboost_stacker(xgb_params, 741)
         '''
-        with tick_tock("add stats features", self.verbose):
+        with tick_tock("stacking", self.verbose):
             d_test = xgboost.DMatrix(self.X_test)
             n_folds = self.kf_n.n_folds
             S_train = np.zeros_like(self.y)
-            S_test_i = np.zeros((self.y.shape[0], n_folds))
+            S_test_i = np.zeros((self.X_test.shape[0], n_folds))
 
             for i, (train_index, val_index) in enumerate(self.kf_n):
+                if verbose > 0:
+                    print('xgboost is stacking fold {0} ....'.format(i+1))
                 X_train_cv, X_val_cv = self.X_train[train_index], self.X_train[val_index]
-                y_train_cv = self.y[train_index]
-
+                y_train_cv, y_valid_cv = self.y[train_index], self.y[val_index]
                 d_train_fold = xgboost.DMatrix(X_train_cv, y_train_cv)
-                d_val_fold = xgboost.DMatrix(X_val_cv)
+                d_val_fold = xgboost.DMatrix(X_val_cv, y_valid_cv)
+                watchlist = [(d_train_fold,'train'),(d_val_fold,'eval')]
 
-                model_fold = xgboost.train(xgb_params, dtrain=d_train_fold, num_boost_round=num_boost_round)
+                model_fold = xgboost.train(xgb_params
+                                           , evals=watchlist
+                                           , verbose_eval=xgboost_verbose_eval
+                                           , early_stopping_rounds=early_stopping_rounds
+                                           , dtrain=d_train_fold
+                                           , num_boost_round=num_boost_round)
                 S_train[val_index] = model_fold.predict(d_val_fold)
                 S_test_i[:, i] = model_fold.predict(d_test)
 
             S_test = S_test_i.sum(axis=1) / n_folds
 
             return S_train, S_test
+
+    def run_nn_stacker(self
+                       , build_model
+                       , epochs
+                       , batch_size
+                       , saved_path
+                       , saved_file_name
+                       , patience
+                       , verbose_nn = 2
+                       , verbose=1):
+        '''Stacking for neural network
+
+           Parameters
+           ----------
+
+           Return
+           ------
+           S_train: numpy array
+                            stacked training data
+           S_test: numpy array
+                            stacked testing data
+
+           Example
+           -------
+            S_train, S_test = run_lgbm_stacker(xgb_params, 741)
+        '''
+        with tick_tock("stacking", self.verbose):
+            n_folds = self.kf_n.n_folds
+            S_train = np.zeros_like(self.y)
+            S_test_i = np.zeros((self.X_test.shape[0], n_folds))
+
+            for i, (train_index, val_index) in enumerate(self.kf_n):
+                model = build_model()
+                if verbose > 0:
+                    print('neural network is stacking fold {0} ....'.format(i+1))
+                X_train_cv, X_val_cv = self.X_train[train_index], self.X_train[val_index]
+                y_train_cv, y_valid_cv = self.y[train_index], self.y[val_index]
+
+                callbacks_ins = callbacks_keras(saved_path + saved_file_name + '_v'+ str(i) + ".p"
+                                                , model, patience=patience)
+                model.fit(X_train_cv,y_train_cv, callbacks=callbacks_ins.callbacks
+                          ,validation_data=[X_val_cv, y_valid_cv]
+                          ,epochs=epochs, batch_size=batch_size, verbose=verbose_nn)
+
+                model.load_weights(saved_path + saved_file_name + '_v'+ str(i) + ".p")
+                S_train[val_index] = np.squeeze(model.predict(X_val_cv))
+                S_test_i[:, i] = np.squeeze(model.predict(self.X_test))
+
+            S_test = S_test_i.sum(axis=1) / n_folds
+            return S_train, S_test
+
     def run_other_stackers(self, base_models):
         '''Stacking for sklearn mdoels
            Parameters
@@ -170,18 +247,17 @@ class ka_stacking_generalization(object):
             print ("Fitting base models begin")
             for i, model in enumerate(base_models):
                 print ("Fitting the {0} th base models".format(i))
-                S_test_i = np.zeros((self.X_test.shape[0], len(self.kf_n)))
+                S_test_i = np.zeros((self.X_test.shape[0], self.kf_n.n_folds))
                 for j, (train_index, val_index) in enumerate(self.kf_n):
                     X_train_cv, X_val_cv = self.X_train[train_index], self.X_train[val_index]
                     y_train_cv = self.y[train_index]
 
                     model.fit(X_train_cv, y_train_cv)
-                    y_pred = model.predict(X_val_cv)[:]
+                    y_pred = model.predict_proba(X_val_cv)[:,1]
                     S_train[val_index,i] = y_pred
-                    S_test_i[:,j] = model.predict(self.X_test)[:]
+                    S_test_i[:,j] = model.predict_proba(self.X_test)[:,1]
                 S_test[:,i] = S_test_i.mean(1)
             return S_train, S_test
-
 
 
 def ka_bagging_2class_or_reg(X_train, y_train, model, seed, bag_round
