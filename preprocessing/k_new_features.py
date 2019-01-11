ec0452e8c072ad377499f01f9728e30e5f874805
kun <972775099@qq.com>
2019-01-11 09:56:59 +0800

    fix groupby features

--- preprocessing/k_new_features.py
+++ preprocessing/k_new_features.py
@@ -1,7 +1,7 @@
 import warnings
 import numpy as np
 import pandas as pd
-from ..Utils.KA_utils import tick_tock
+from ..utils.k_others import tick_tock
 
 def _deprecated(func):
     '''This is a decorator which can be used to mark functions
@@ -16,62 +16,6 @@ def _deprecated(func):
     new_func.__dict__.update(func.__dict__)
     return new_func
 
-@_deprecated
-def ka_add_groupby_features_n_vs_1(df, group_columns_list, target_columns_list, methods_list, keep_only_stats=True, verbose=1):
-    '''Create statistical columns, group by [N columns] and compute stats on [1 column]
-
-       Parameters
-       ----------
-       df: pandas dataframe
-          Features matrix
-       group_columns_list: list_like
-          List of columns you want to group with, could be multiple columns
-       target_columns_list: list_like
-          column you want to compute stats, need to be a list with only one element
-       methods_list: list_like
-          methods that you want to use, all methods that supported by groupby in Pandas
-       keep_only_stats: boolean
-          only keep stats or return both raw columns and stats
-       verbose: int
-          1 return tick_tock info 0 do not return any info
-       Return
-       ------
-       new pandas dataframe with original columns and new added columns
-
-       Example
-       -------
-       ka_add_stats_features_n_vs_1(train, group_columns_list=['x0'], target_columns_list=['x10'])
-    '''
-    with tick_tock("add stats features", verbose):
-        dicts = {"group_columns_list": group_columns_list , "target_columns_list": target_columns_list, "methods_list" :methods_list}
-
-        for k, v in dicts.items():
-            try:
-                if type(v) == list:
-                    pass
-                else:
-                    raise TypeError(k + "should be a list")
-            except TypeError as e:
-                print(e)
-                raise
-
-        grouped_name = ''.join(group_columns_list)
-        target_name = ''.join(target_columns_list)
-        combine_name = [[grouped_name] + [method_name] + [target_name] for method_name in methods_list]
-
-        df_new = df.copy()
-        grouped = df_new.groupby(group_columns_list)
-
-        the_stats = grouped[target_name].agg(methods_list).reset_index()
-        the_stats.columns = [grouped_name] + \
-                            ['_%s_%s_by_%s' % (grouped_name, method_name, target_name) \
-                             for (grouped_name, method_name, target_name) in combine_name]
-        if keep_only_stats:
-            return the_stats
-        else:
-            df_new = pd.merge(left=df_new, right=the_stats, on=group_columns_list, how='left')
-        return df_new
-
 def ka_create_groupby_features(df, group_columns_list, method_dict, add_to_original_data=False, verbose=1, verbose_detail="create stats features",):
     '''Create statistical columns, group by [N columns] and compute stats on [N column]
 
@@ -83,6 +27,7 @@ def ka_create_groupby_features(df, group_columns_list, method_dict, add_to_origi
           List of columns you want to group with, could be multiple columns
        method_dict: python dictionary
           Dictionay used to create stats variables
+          shoubld be {'feature': ['method_1', 'method_2']}, if method is a lambda, use function inplace.
        add_to_original_data: boolean
           only keep stats or add stats variable to raw data
        verbose: int
@@ -119,7 +64,7 @@ def ka_create_groupby_features(df, group_columns_list, method_dict, add_to_origi
         grouped = df_new.groupby(group_columns_list)
 
         the_stats = grouped.agg(method_dict)
-        the_stats.columns = [''.join(group_columns_list) + '_G_' +"_".join(x) for x in the_stats.columns.ravel()]
+        the_stats.columns = [''.join(group_columns_list) + '_LV_' +"_".join(x[::-1]) for x in the_stats.columns.ravel()]
         the_stats.reset_index(inplace=True)
         if not add_to_original_data:
             df_new = the_stats
@@ -128,6 +73,7 @@ def ka_create_groupby_features(df, group_columns_list, method_dict, add_to_origi
 
     return df_new
 
+@_deprecated
 def ka_create_groupby_features_old(df, group_columns_list, agg_dict, keep_only_stats=True, verbose=1):
     '''Create statistical columns, group by [N columns] and compute stats on [N column]
        Parameters
